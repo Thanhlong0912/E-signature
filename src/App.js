ea0ac96156163586c6520e44c3e567d0f81ba463
Thanhlong0912 <dinhthanhlong47@gmail.com>
2022-10-06 11:13:47 +0700

    E-signature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState} from 'react';
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import SignaturePad from "react-signature-canvas";
+import "./signatureCanvas.css";
 
 function App() {
+  const [imageURL, setImageURL] = useState(null);
+  const sigCanvas = useRef({});
+  const handeClear = () =>  sigCanvas.current.clear();
+  // const handleSave = () => console.log(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
+  const handleSave = () => setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-signature</h1>
+
+      <Popup modal trigger={<button>Show</button>} closeOnDocumentClick={false}>
+        { close => (
+          <>
+            <SignaturePad
+              ref={sigCanvas}
+              canvasProps={{
+                className: "signatureCanvas",
+              }}
+            />
+            <button onClick={close}>Close</button>
+            <button onClick={handeClear}>Clear</button>
+            <button onClick={handleSave}>Save</button>
+          </>
+        )}
+      </Popup>
+
+      <br />
+      <br />
+
+      {imageURL ? (<img
+      src={imageURL}
+      alt="mi firma"
+      style={{
+        display: "block",
+        margin: "0 auto",
+        border: "1px solid black",
+        width: "300px",
+      }}
+      />) : null }
     </div>
   );
 }
